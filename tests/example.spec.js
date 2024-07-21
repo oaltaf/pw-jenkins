b419b5ef55cbdf4387feeb32ad3da3dfc4d64f79
oaltaf <osama.altaf385@gmail.com>
2024-07-21 12:32:50 +0500

    new commit

--- tests/example.spec.js
+++ tests/example.spec.js
@@ -2,8 +2,9 @@
 const { test, expect } = require('@playwright/test');
 
 test('has title', async ({ page }) => {
+  test.setTimeout(120000);
   await page.goto('https://playwright.dev/');
-  await page.waitForTimeout(2000)
+  await page.waitForTimeout(30000)
 
   // Expect a title "to contain" a substring.
   await expect(page).toHaveTitle(/Playwright/);
