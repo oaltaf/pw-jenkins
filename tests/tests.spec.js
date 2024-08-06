79969652db2ab9dc481ce9dbe8fda5d0cbbc14da
oaltaf <osama.altaf385@gmail.com>
2024-08-06 21:36:46 +0500

    changes

---
+++ tests/tests.spec.js
@@ -0,0 +1,21 @@
+// @ts-check
+const { test, expect } = require('@playwright/test');
+
+test('has title', async ({ page }) => {
+  // test.setTimeout(120000);
+  await page.goto('https://playwright.dev/');
+  // await page.waitForTimeout(2000)
+
+  // Expect a title "to contain" a substring.
+  await expect(page).toHaveTitle(/Playwright/);
+});
+
+test('get started link', async ({ page }) => {
+  await page.goto('https://playwright.dev/');
+
+  // Click the get started link.
+  await page.getByRole('link', { name: 'Get started' }).click();
+
+  // Expects page to have a heading with the name of Installation.
+  await expect(page.getByRole('heading', { name: 'Installation' })).toBeVisible();
+});
